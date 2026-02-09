5aef266d7b56f54d2d033b3b95d155f728fcd1d4
Alexandre Corlet <corlet.xande@gmail.com>
2026-02-08 21:21:53 -0300

    update: Optimized Radix Sort algorithm memory usage.

--- src/radix_sort.cpp
+++ src/radix_sort.cpp
@@ -1,86 +1,74 @@
-#include "radix_sort.h"
-#include <vector>
 #include <algorithm>
 #include <cstdint>
 #include <cstring> 
+#include <vector>
+#include "radix_sort.h"
 
 using namespace std;
 
-const int NUM_PASSES = 8;
-const int MAXN = 256;
+// Required number of passes to go through each byte of the type double (64 bits = 8 bytes).
+const int NUM_PASSES = sizeof(double);
+
+// The radix (base): 256 because we process 1 byte at a time.
+const int RADIX = 256;
+
+// Mask to extract the byte we want to analyze (or sort)
 const int BYTE_MASK = 0xFF;
+
+// Mask to extract the MSB and to check if the number is positive or negative
 const uint64_t MSB_MASK = 1ULL << 63;
 
-struct RadixBuffers {
+// Number of bits we need to shift to go to the next byte (1 << BITS_PER_BYTE_SHIFT = 8)
+const int BITS_PER_BYTE_SHIFT = 3;
 
-    vector<uint64_t> bytes, result_bytes;
-    vector<double> result;
-    vector<int> count;
-
-    RadixBuffers(int n) {
-        bytes.resize(n);
-        result_bytes.resize(n);
-        result.resize(n);
-        count.resize(MAXN);
-    }
-
-};
-
-inline uint64_t double_to_byte(double num) {
-
+// Converts a double to a byte (uint64)
+inline uint64_t double_to_uint64(double num) {
     uint64_t byt;
     memcpy(&byt, &num, sizeof(double));
 
     return (byt & MSB_MASK) ? ~byt : byt ^ MSB_MASK;
 }
 
-inline int extract_byte(uint64_t val, int pass) {
-    return (val >> (pass << 3)) & BYTE_MASK;
-}
+void radix_sort::radix_sort(vector<double> &arr) {
+    int n = (int) arr.size();
 
-void byte_sort(vector<double> &vec, RadixBuffers &buf, int pass) {
-
-    int n = vec.size();
-    
-    // Reset counter buffer
-    fill(buf.count.begin(), buf.count.end(), 0);
-
-    for (int i = 0; i < n; ++i) {
-        int curr_byte = extract_byte(buf.bytes[i], pass);
-        ++buf.count[curr_byte];
-    }
-
-    // Compute the offset of the i-th byte
-    for (int i = 1; i < MAXN; ++i) {
-        buf.count[i] += buf.count[i - 1];
-    }
-    
-    for (int i = n - 1; i >= 0; --i) {
-        int curr_byte = extract_byte(buf.bytes[i], pass);
-        int idx = --buf.count[curr_byte];
-        buf.result[idx] = vec[i];
-        buf.result_bytes[idx] = buf.bytes[i];
-    }
-
-    swap(vec, buf.result);
-    swap(buf.bytes, buf.result_bytes);
-}
-
-void radix_sort(vector<double> &vec) {
-
-    int n = vec.size();
-    if (n == 0) {
+    if (n < 2) {
         return;
     }
 
-    RadixBuffers buf(n);
+    // Temporary buffer to store sorted bytes
+    vector<double> temp_buffer(arr.size());
+        
+    vector<double> *src = &arr;
+    vector<double> *dest = &temp_buffer;
 
-    // Converts each double value in vec to its byte representation
-    // and stores the result in the bytes vector
-    transform(vec.begin(), vec.end(), buf.bytes.begin(), double_to_byte);
-    
-    for (int pass = 0; pass < NUM_PASSES; ++pass) {
-        byte_sort(vec, buf, pass);
+    for (int p = 0; p < NUM_PASSES; p++) {
+        // Compute frequency of each element
+        size_t count[RADIX];
+        memset(count, 0, sizeof(count));
+        for (int i = 0; i < n; i++) {
+            uint64_t byte_val = double_to_uint64((*src)[i]);
+            int byte_idx = (byte_val >> (p << BITS_PER_BYTE_SHIFT)) & BYTE_MASK;
+            count[byte_idx]++;
+        }
+
+        // Compute offset
+        for (int i = 1; i < RADIX; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            uint64_t byte_val = double_to_uint64((*src)[i]);
+                
+            int byte_idx = (byte_val >> (p << BITS_PER_BYTE_SHIFT)) & BYTE_MASK;
+            int pos = --count[byte_idx];
+            (*dest)[pos] = (*src)[i];
+        }
+
+        swap(src, dest);
     }
 
+    if (src != &arr) {
+        arr = move(*src);
+    }
 }
--- src/radix_sort.h
+++ src/radix_sort.h
@@ -1,4 +1,11 @@
 #pragma once
 #include <vector>
 
-void radix_sort(std::vector<double> &arr);
+namespace radix_sort {
+
+    // Apply the optimized Radix Sort routine to sort an array
+    // of doubles in non-decreasing order. This procedure only works
+    // if the double follows the IEE754 pattern
+    void radix_sort(std::vector<double> &arr);
+
+}   // radix_sort
