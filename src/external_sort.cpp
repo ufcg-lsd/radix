465d728da92e86c74142e77e6dadbe7a718c0701
alexandrecorlet <corlet.xande@gmail.com>
2026-02-21 18:32:09 -0300

    Fix bug in external sort. Update max elements expression to consider auxiliary memory used by Radix

--- src/external_sort.cpp
+++ src/external_sort.cpp
@@ -63,7 +63,9 @@ namespace external_sort {
 
         // Compute buffer size
         size_t safe_payload = (size_t) (this->memory_limit_bytes * SAFETY_MARGIN);
-        size_t max_elements = safe_payload / sizeof(double);
+        // limit Radix usage to make the comparison between the algorithms fair
+        size_t memory_usage_factor = (this->sort_strategy == RADIX_SORT) ? 3 : 1;
+        size_t max_elements = safe_payload / (memory_usage_factor *sizeof(double));
 
         vector<double> buffer;
         buffer.reserve(max_elements);
